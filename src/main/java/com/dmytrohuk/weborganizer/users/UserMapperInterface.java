87aeee2f8adfdac260d6c66dcf23d7227e53964c
DMGUK <dmytro.guk@gmail.com>
2023-04-26 17:25:07 +0300

    pom.xml issue(Failed to execute goal org.apache.maven.plugins:maven-deploy-plugin:3.0.0:deploy)

--- src/main/java/com/dmytrohuk/weborganizer/users/UserMapperInterface.java
+++
@@ -1,7 +0,0 @@
-package com.dmytrohuk.weborganizer.users;
-
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface UserMapperInterface {
-}
--- src/main/java/com/dmytrohuk/weborganizer/users/UserService.java
+++ src/main/java/com/dmytrohuk/weborganizer/users/UserService.java
@@ -3,7 +3,7 @@ package com.dmytrohuk.weborganizer.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import com.dmytrohuk.weborganizer.users.User;
 import java.util.List;
 import java.util.Optional;
 
