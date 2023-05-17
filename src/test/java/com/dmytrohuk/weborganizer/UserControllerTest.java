39a15b1d594828b087f33f03dc6d571a7f7a0e4d
DMGUK <dmytro.guk@gmail.com>
2023-05-17 18:30:06 +0300

    SQL script does not execute

--- src/test/java/com/dmytrohuk/weborganizer/UserControllerTest.java
+++ src/test/java/com/dmytrohuk/weborganizer/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.dmytrohuk.weborganizer;
 import com.dmytrohuk.weborganizer.users.User;
 import com.dmytrohuk.weborganizer.users.UserRepository;
 import com.dmytrohuk.weborganizer.users.UserService;
+import org.aspectj.weaver.patterns.IToken;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.CannotReadScriptException;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,13 +28,19 @@ import org.testcontainers.shaded.org.hamcrest.Matchers;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.testcontainers.shaded.org.hamcrest.collection.IsMapWithSize.aMapWithSize;
 
@@ -42,25 +58,59 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeTestClass
+    private void executeSqlScript() throws Exception{
+        final String JDBC_URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
+        final String JDBC_USERNAME = "sa";
+        final String JDBC_PASSWORD = "";
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD)) {
+            try {
+//                String scriptPath1 = "src/test/java/resources/init/create-table.sql";
+//                Resource scriptResource1 = new ClassPathResource(scriptPath1);
+//                ScriptUtils.executeSqlScript(connection, scriptResource1);
+                String scriptPath2 = "src/test/java/resources/empty/reset.sql";
+                Resource scriptResource2 = new ClassPathResource(scriptPath2);
+                ScriptUtils.executeSqlScript(connection, scriptResource2);
+                String scriptPath3 = "src/test/java/resources/init/user-data.sql";
+                Resource scriptResource3 = new ClassPathResource(scriptPath3);
+                ScriptUtils.executeSqlScript(connection, scriptResource3);
+            } catch (Exception e) {
+                new IllegalStateException("The file does not exist");
+            }
+        }
+    }
+
     @Test
-    @SqlGroup({
-            @Sql(value = "classpath:empty/reset.sql", executionPhase = BEFORE_TEST_METHOD),
-            @Sql(value = "classpath:init/user-data.sql", executionPhase = BEFORE_TEST_METHOD)
-    })
+    @WithMockUser(username = "username")
     void testCreateUser() throws Exception{
-        final File jsonFile = new ClassPathResource("init/user.json").getFile();
-        final String userToCreate = Files.readString(jsonFile.toPath());
+        final String JDBC_URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
+        final String JDBC_USERNAME = "username";
+        final String JDBC_PASSWORD = "password";
+
+        final File jsonFile1 = new FileSystemResource("src/test/java/resources/init/user.json").getFile();
+        final String userToCreate = Files.readString(jsonFile1.toPath());
 
         this.mockMvc.perform(
             post("/api/users")
-            .contentType(MediaType.APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
             .content(userToCreate)
         )
         .andDo(print())
-        .andExpect(status().isCreated())
-        .andExpect((ResultMatcher) jsonPath("$").isMap())
-        .andExpect((ResultMatcher) jsonPath("$", aMapWithSize(8)));
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$").isMap());
 
-        assertEquals(this.userRepository.findAll().size(), 6);
+        assertNotEquals(this.userRepository.findByUsername("username6").getId(), 6);
+    }
+    @Test
+    @WithMockUser(username = "username")
+    void testGetUserById() throws Exception{
+        final File jsonFile1 = new FileSystemResource("src/test/java/resources/init/user.json").getFile();
+        final String userToCreate = Files.readString(jsonFile1.toPath());
+
+        this.mockMvc.perform(get("/api/users/{id}", 6))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").value(6));
     }
 }
--- src/test/java/resources/init/user-data.sql
+++ src/test/java/resources/init/user-data.sql
@@ -1,16 +1,16 @@
-INSERT INTO user (id, username, password, first_name, surname, email, address, role)
+INSERT INTO users (id, username, password, first_name, surname, email, address, role)
 VALUES (1, 'username1', 'password1', 'Firstname', 'Surname',
         'email.abc1@emai.com', '123 Main Str.', 'USER');
-INSERT INTO user (id, username, password, first_name, surname, email, address, role)
+INSERT INTO users (id, username, password, first_name, surname, email, address, role)
 VALUES (2, 'username2', 'password2', 'Firstname', 'Surname',
         'email.abc2@emai.com', '123 Main Str.', 'USER');
-INSERT INTO user (id, username, password, first_name, surname, email, address, role)
+INSERT INTO users (id, username, password, first_name, surname, email, address, role)
 VALUES (3, 'username3', 'password3', 'Firstname', 'Surname',
         'email.abc3@emai.com', '123 Main Str.', 'USER');
-INSERT INTO user (id, username, password, first_name, surname, email, address, role)
+INSERT INTO users (id, username, password, first_name, surname, email, address, role)
 VALUES (4, 'username4', 'password4', 'Firstname', 'Surname',
         'email.abc4@emai.com', '123 Main Str.', 'USER');
-INSERT INTO user (id, username, password, first_name, surname, email, address, role)
+INSERT INTO users (id, username, password, first_name, surname, email, address, role)
 VALUES (5, 'username5', 'password5', 'Firstname', 'Surname',
         'email.abc5@emai.com', '123 Main Str.', 'USER');
 
